aed9abe72d275c9080b769351546961893a9ad13
Swornim Chhetri <schnitzelplays.nerd@gmail.com>
2022-02-01 12:44:08 +0545

    AvatarURL

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const Discord = require("discord.js")
 require("dotenv").config()
 
+const generateImage = require("./generateImage")
+
 const client = new Discord.Client({
     intents:[
         "GUILDS",
-        "GUILD_MESSAGES"
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
     ]
 })
 
@@ -18,6 +21,14 @@ client.on("messageCreate", (message) => {
     }else if ( message.content == "Ping!"){
         message.reply(":ping_pong: Pong!")
     } 
+})
+
+const welcomeChannelId = "937956422346477628"
+client.on("guildMemberAdd",async (member) => {
+    const img = await generateImage(member)
+    member.guild.channels.cache.get(welcomeChannelId).send({
+        content: `<@${member.id}> Welcome to the server!`,
+        files: [img]})  
 })
 
 client.login(process.env.TOKEN) 
